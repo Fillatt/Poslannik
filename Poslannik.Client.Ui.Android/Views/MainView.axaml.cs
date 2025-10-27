21c401944dc0a721476841b72e644acd61f3b280
nedemack <145268981+nedemack@users.noreply.github.com>
2025-10-27 21:59:08 +0300

    Add 3 window

--- Poslannik.Client.Ui.Android/Views/MainView.axaml.cs
+++ Poslannik.Client.Ui.Android/Views/MainView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Poslannik.Client.Ui.Android.Views
 {
@@ -87,6 +89,15 @@ namespace Poslannik.Client.Ui.Android.Views
                 case GroupChatView groupChatView:
                     SetupGroupChatView(groupChatView);
                     break;
+                case NewChatView newChatView:
+                    SetupNewChatView(newChatView);
+                    break;
+                case UserProfileView userProfileView:
+                    SetupUserProfileView(userProfileView);
+                    break;
+                case ParticipantsView participantsView:
+                    SetupParticipantsView(participantsView);
+                    break;
             }
         }
 
@@ -125,6 +136,7 @@ namespace Poslannik.Client.Ui.Android.Views
         private void SetupChatsView(ChatsView view)
         {
             var profileTab = view.FindControl<Button>("ProfileTabButton");
+            var addChatButton = view.FindControl<Button>("AddChatButton");
             var chat1Button = view.FindControl<Button>("Chat1Button");
             var chat2Button = view.FindControl<Button>("Chat2Button");
             var chat3Button = view.FindControl<Button>("Chat3Button");
@@ -134,6 +146,11 @@ namespace Poslannik.Client.Ui.Android.Views
             if (profileTab != null)
             {
                 profileTab.Click += (s, e) => NavigateTo(new ProfileView());
+            }
+
+            if (addChatButton != null)
+            {
+                addChatButton.Click += (s, e) => NavigateToWithHistory(new NewChatView());
             }
 
             if (chat1Button != null)
@@ -185,20 +202,166 @@ namespace Poslannik.Client.Ui.Android.Views
         private void SetupChatView(ChatView view)
         {
             var backButton = view.FindControl<Button>("BackButton");
+            var userAvatarButton = view.FindControl<Border>("UserAvatarBorder");
 
             if (backButton != null)
             {
                 backButton.Click += (s, e) => NavigateBack();
+            }
+
+            if (userAvatarButton == null)
+            {
+                var borders = view.GetVisualDescendants().OfType<Border>();
+                foreach (var border in borders)
+                {
+                    if (border.Width == 35 && border.Height == 35 &&
+                        border.CornerRadius == new Avalonia.CornerRadius(17.5))
+                    {
+                        userAvatarButton = border;
+                        break;
+                    }
+                }
+            }
+
+            if (userAvatarButton != null)
+            {
+                userAvatarButton.PointerPressed += (s, e) => NavigateToWithHistory(new UserProfileView());
             }
         }
 
         private void SetupGroupChatView(GroupChatView view)
         {
             var backButton = view.FindControl<Button>("BackButton");
+            var groupInfoButton = view.FindControl<Button>("GroupInfoButton");
 
             if (backButton != null)
             {
                 backButton.Click += (s, e) => NavigateBack();
+            }
+
+            if (groupInfoButton != null)
+            {
+                groupInfoButton.Click += (s, e) => NavigateToWithHistory(new ParticipantsView());
+            }
+        }
+
+        private void SetupNewChatView(NewChatView view)
+        {
+            var backButton = view.FindControl<Button>("BackButton");
+            var chatsTabButton = view.FindControl<Button>("ChatsTabButton");
+            var profileTabButton = view.FindControl<Button>("ProfileTabButton");
+            var createChatButton = view.FindControl<Button>("CreateChatButton");
+
+            if (backButton != null)
+            {
+                backButton.Click += (s, e) => NavigateBack();
+            }
+
+            if (chatsTabButton != null)
+            {
+                chatsTabButton.Click += (s, e) => NavigateTo(new ChatsView());
+            }
+
+            if (profileTabButton != null)
+            {
+                profileTabButton.Click += (s, e) => NavigateTo(new ProfileView());
+            }
+
+            if (createChatButton != null)
+            {
+                createChatButton.Click += (s, e) =>
+                {
+                    _navigationStack.Clear();
+                    NavigateTo(new ChatsView());
+                };
+            }
+        }
+
+        private void SetupUserProfileView(UserProfileView view)
+        {
+            var backButton = view.FindControl<Button>("BackButton");
+            var chatsTabButton = view.FindControl<Button>("ChatsTabButton");
+            var profileTabButton = view.FindControl<Button>("ProfileTabButton");
+
+            if (backButton != null)
+            {
+                backButton.Click += (s, e) => NavigateBack();
+            }
+
+            if (chatsTabButton != null)
+            {
+                chatsTabButton.Click += (s, e) => NavigateTo(new ChatsView());
+            }
+
+            if (profileTabButton != null)
+            {
+                profileTabButton.Click += (s, e) => NavigateTo(new ProfileView());
+            }
+        }
+
+        private void SetupParticipantsView(ParticipantsView view)
+        {
+            var backButton = view.FindControl<Button>("BackButton");
+            var chatsTabButton = view.FindControl<Button>("ChatsTabButton");
+            var profileTabButton = view.FindControl<Button>("ProfileTabButton");
+            var addParticipantButton = view.FindControl<Button>("AddParticipantButton");
+            var deleteChatButton = view.FindControl<Button>("DeleteChatButton");
+            var leaveChatButton = view.FindControl<Button>("LeaveChatButton");
+            var myProfileButton = view.FindControl<Button>("MyProfileButton");
+            var participant1Button = view.FindControl<Button>("Participant1Button");
+            var participant2Button = view.FindControl<Button>("Participant2Button");
+
+            if (backButton != null)
+            {
+                backButton.Click += (s, e) => NavigateBack();
+            }
+
+            if (chatsTabButton != null)
+            {
+                chatsTabButton.Click += (s, e) => NavigateTo(new ChatsView());
+            }
+
+            if (profileTabButton != null)
+            {
+                profileTabButton.Click += (s, e) => NavigateTo(new ProfileView());
+            }
+
+            if (addParticipantButton != null)
+            {
+                addParticipantButton.Click += (s, e) => NavigateToWithHistory(new NewChatView());
+            }
+
+            if (deleteChatButton != null)
+            {
+                deleteChatButton.Click += (s, e) =>
+                {
+                    _navigationStack.Clear();
+                    NavigateTo(new ChatsView());
+                };
+            }
+
+            if (leaveChatButton != null)
+            {
+                leaveChatButton.Click += (s, e) =>
+                {
+                    _navigationStack.Clear();
+                    NavigateTo(new ChatsView());
+                };
+            }
+
+            if (myProfileButton != null)
+            {
+                myProfileButton.Click += (s, e) => NavigateToWithHistory(new UserProfileView());
+            }
+
+            if (participant1Button != null)
+            {
+                participant1Button.Click += (s, e) => NavigateToWithHistory(new UserProfileView());
+            }
+
+            if (participant2Button != null)
+            {
+                participant2Button.Click += (s, e) => NavigateToWithHistory(new UserProfileView());
             }
         }
     }
